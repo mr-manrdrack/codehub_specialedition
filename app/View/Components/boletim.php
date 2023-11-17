0d43a952ca199d82e4933570895887e586e8fc70
IgorCN <icnigor1512@gmail.com>
2023-11-17 05:06:45 -0300

    boletim dinamismo incompleto

--- app/View/Components/boletim.php
+++ app/View/Components/boletim.php
@@ -12,34 +12,23 @@ class boletim extends Component
      * Create a new component instance.
      */
     public $nomedisciplina, $mediaanual, $notarecfinal, $notafinal;
-    public $listanotas, $listafj, $listafnj;
-    public $listanotasS, $listafj2, $listafnj2;
-    public $listanotas3, $listafj3, $listafnj3;
-    public $listanotas4, $listafj4, $listafnj4;
+    public $listanotas, $listafj, $listafnj, $listanotasa;
     public $listaPFAP, $listaPFAS, $listaPFAT, $listaPFAQ;
     
-    public function __construct($nomedisciplina,$listaPFAP,$listaPFAS,$listaPFAT,$listaPFAQ,$mediaanual,$notarecfinal,$notafinal, $listanotas, $listafj, $listafnj, $listanotasS, $listafj2, $listafnj2, $listanotas3, $listafj3, $listafnj3, $listanotas4, $listafj4, $listafnj4)
+    public function __construct($listanotasa,$nomedisciplina,$listaPFAP,$listaPFAS,$listaPFAT,$listaPFAQ,$mediaanual,$notarecfinal,$notafinal, $listanotas, $listafj, $listafnj)
     {
         $this->nomedisciplina = $nomedisciplina;
+        $this->listanotasa = $listanotasa;
 
         $this->listanotas = $listanotas;
         $this->listafj = $listafj;
         $this->listafnj = $listafnj;
         $this->listaPFAP = $listaPFAP;
         
-        $this->listanotasS = $listanotasS;
-        $this->listafj2 = $listafj2;
-        $this->listafnj2 = $listafnj2;
         $this->listaPFAS = $listaPFAS;
 
-        $this->listanotas3 = $listanotas3;
-        $this->listafj3 = $listafj3;
-        $this->listafnj3 = $listafnj3;
         $this->listaPFAT = $listaPFAT;
 
-        $this->listanotas4 = $listanotas4;
-        $this->listafj4 = $listafj4;
-        $this->listafnj4 = $listafnj4;
         $this->listaPFAQ = $listaPFAQ;
 
         $this->mediaanual = $mediaanual;
--- resources/views/aluno/boletim.blade.php
+++ resources/views/aluno/boletim.blade.php
@@ -15,7 +15,7 @@
         <x-navbar home="/aluno" opcao1="Boletim" caminhoOpcao1="boletim" opcao2="Horário" caminhoOpcao2="horario" opcao3="Cardápio" caminhoOpcao3="cardapio" opcao4="Eventos" caminhoOpcao4="eventos" opcao5="Bloco de Notas" caminhoOpcao5="blocodenotas" opcao6="" caminhoOpcao6="usuario" />
     </nav>
     <x-titulo titulo="Boletim:" subtitulo="" />
-    <x-boletim :nomedisciplina="$nomedisciplina" :listanotas="$listanotas" :listafj="$listafj" :listafnj="$listafnj" :listaPFAP="$listaPFAP" :listanotasS="$listanotas2" :listafj2="$listafj2" :listafnj2="$listafnj2" :listaPFAS="$listaPFAS" :listanotas3="$listanotas3" :listafj3="$listafj3" :listafnj3="$listafnj3" :listaPFAT="$listaPFAT" :listanotas4="$listanotas4" :listafj4="$listafj4" :listafnj4="$listafnj4" :listaPFAQ="$listaPFAQ" :mediaanual="$mediaanual" :notarecfinal="$notarecfinal" :notafinal="$notafinal"/>
+    <x-boletim :listanotasa="$listanotasa" :nomedisciplina="$nomedisciplina" :listanotas="$listanotas" :listafj="$listafj" :listafnj="$listafnj" :listaPFAP="$listaPFAP" :listaPFAS="$listaPFAS"  :listaPFAT="$listaPFAT" :listaPFAQ="$listaPFAQ" :mediaanual="$mediaanual" :notarecfinal="$notarecfinal" :notafinal="$notafinal"/>
     <x-footer />
 </body>
 </html>
